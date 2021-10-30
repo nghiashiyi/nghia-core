c57b388f9f5f771fb1d42cbb7fa2adea4a22c192
nghia <nghia.tsy@gmail.com>
2021-10-30 22:18:30 +0700

    update fonts

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,12 @@ import 'package:adroit_flutter/core/utils/flavor_config.dart';
 import 'package:adroit_flutter/core/utils/utils.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import 'di.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  await Firebase.initializeApp();
   await FlavorConfig.instance.init(Flavors.prod);
 
   setupLocator(flavorConfig: FlavorConfig.instance);
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -5,13 +5,11 @@ import 'package:adroit_flutter/core/utils/utils.dart';
 import 'package:adroit_flutter/di.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  await Firebase.initializeApp();
   await FlavorConfig.instance.init(Flavors.dev);
   Bloc.observer = AppBlocDelegate();
 

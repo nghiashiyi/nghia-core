ad3d5b89ceaffdb5c4ec7f0acb36589a39a745da
nghia <nghia.tsy@gmail.com>
2021-10-30 21:25:24 +0700

    initial how to use localization

--- lib/core/utils/app_locale.dart
+++ lib/core/utils/app_locale.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+/// How to use:
+/// LocaleTexts.appName.tr()
+///
+/// Example:
+/// ```
+/// import 'package:adroit_flutter/core/utils/utils.dart';
+///
+/// ...
+///   @override
+///   Widget build(BuildContext context) {
+///     return Container(
+///       child: Text(LocaleTexts.appName.tr())
+///     );
+///     return MaterialApp(
+///       localizationsDelegates: context.localizationDelegates,
+///       supportedLocales: context.supportedLocales,
+///       locale: context.locale,
+///       theme: ThemeData(
+///         fontFamily: "Montserrat",
+///       ),
+///       initialRoute: Routes.main,
+///       onGenerateRoute: generateRoute,
+///       debugShowCheckedModeBanner: false,
+///       navigatorKey: NavigationService.globalNavigatorKey,
+///     );
+///   }
+/// ```
+///
 class LocaleTexts {
   static const appName = 'appName';
+  static const welcomeTo = 'welcomeTo';
 }
 
 class AppLocales {
